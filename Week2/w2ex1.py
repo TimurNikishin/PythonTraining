73e81970a0cef4acd3845d7e73dffa93a8c4b5eb
timur <timur13@gmail.com>
2019-12-18 15:22:58 +0000

    message

--- Week2/w2ex1.py
+++ Week2/w2ex1.py
@@ -1,10 +1,10 @@
-addr = raw_input("\n\nPlease enter IP address: ")
+addr = input("\n\nPlease enter IP address: ")
 octets = addr.split(".")[0:3]
 octets.append("0")
 subnet = ".".join(octets)
-print "\n\nThe subnet is: %s" % subnet
-print "\n\n%-20s %-20s %-20s" % ("NETWORK_NUMBER", "FIRST_OCTET_BINARY", "FIRST_OCTET_HEX")
-print "%-20s %-20s %-20s" % (subnet, bin(int(octets[0])), hex(int(octets[0])))
+print("\n\nThe subnet is: %s" % subnet)
+print("\n\n%-20s %-20s %-20s" % ("NETWORK_NUMBER", "FIRST_OCTET_BINARY", "FIRST_OCTET_HEX"))
+print("{:^20}{:^20}{:^20}".format(subnet, bin(int(octets[0])), hex(int(octets[0]))))
 
 
 
