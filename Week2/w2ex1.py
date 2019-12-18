b3e4f1ce129220b970e5028fcfbede8d8d709852
timur <timur13@gmail.com>
2019-12-18 15:25:02 +0000

    message

--- Week2/w2ex1.py
+++ Week2/w2ex1.py
@@ -5,6 +5,7 @@ subnet = ".".join(octets)
 print("\n\nThe subnet is: %s" % subnet)
 print("\n\n%-20s %-20s %-20s" % ("NETWORK_NUMBER", "FIRST_OCTET_BINARY", "FIRST_OCTET_HEX"))
 print("{:^20}{:^20}{:^20}".format(subnet, bin(int(octets[0])), hex(int(octets[0]))))
+print("{:^20}{:^20}{:^20}".format(subnet, bin(int(octets[0])), hex(int(octets[0]))))
 
 
 
